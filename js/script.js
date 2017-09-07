85a267c1aa8955fb248593113bf80b3bba060301
kibirev (Виктор Кибирев) <kibirev@office.local>
2017-09-07 14:44:39 +0300

    JS script was updated, shadow options were edited

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-var openLink = document.querySelector(".open-form-button");
-var findForm = document.querySelector(".find-hotel-form");
+(function () {
+  'use srtict';
+  var openLink = document.querySelector(".open-form-button");
+  var findForm = document.querySelector(".find-hotel-form");
 
-findForm.classList.add("find-hotel-form-close");
-
-openLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  findForm.classList.toggle("find-hotel-form-close");
-});
+  if(openLink) {
+    openLink.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      findForm.classList.toggle("find-hotel-form-close");
+    });
+  }  
+})();
