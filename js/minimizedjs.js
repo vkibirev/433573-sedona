e2cc08d269ce66d66bfa8475533fb67a78412978
unknown <vitya.kibirev@gmail.com>
2017-09-09 15:15:32 +0300

    changes for finish checking

---
+++ js/minimizedjs.js
@@ -0,0 +1,1 @@
+(function(){'use srtict';var openLink=document.querySelector(".open-form-button");var findForm=document.querySelector(".find-hotel-form");if(openLink){openLink.addEventListener("click",function(evt){evt.preventDefault();findForm.classList.toggle("find-hotel-form-close")})}})()
